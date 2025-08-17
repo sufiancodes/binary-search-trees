8245dd032ad2aa64b5e30ca2cf082c1dc1ba680d
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-08-18 04:46:32 +0500

    Add method build tree
    build_tree method add to convert array in to balanced bst and return root

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,5 +1,18 @@
+require_relative 'node'
 class Tree
-  def initialize
-    
+  attr_accessor :root, :arry
+  def initialize(array)
+    @arry = array
+  end
+  def build_tree (arr, start, stop)
+    array = arr.sort.uniq
+    return nil if start > stop
+    mid = (start+stop) / 2
+    root = Node.new(array[mid])
+    root.left = build_tree(array, start, mid-1 )
+    root.right = build_tree(array, mid+1, stop)
+    return root
   end
 end
+
+bst = Tree.new(arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
